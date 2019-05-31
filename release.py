c26276369733581c1b3cddf55b51f5501c4feeea
Thijs Withaar <thijs.withaar@gmail.com>
2019-05-31 19:58:59 +0200

    local build script

--- release.py
+++ release.py
@@ -2,27 +2,34 @@
 import os
 from collections import namedtuple
 
-Config = namedtuple('Config', ('DOCKER_IMAGE','DOCKER_FILE','DOCKER_SYSTEM'))
-
-
-configs = (
-    Config('thijswithaar/ubuntu:local', 'Dockerfile.ubuntu', 'ubuntu'),
-    Config('thijswithaar/fedora:local', 'Dockerfile.fedora_rawhide', 'fedora'),
-)
-
-
 def run(cmd):
     print('# ' + cmd)
-    return os.system(cmd)
+    os.system(cmd)
+
+
+Config = namedtuple('Config', ('DOCKER_IMAGE','DOCKER_FILE','DOCKER_SYSTEM'))
+
+configs = (
+    Config('thijswithaar/ubuntu:devel', 'Dockerfile.ubuntu', 'Ubuntu'),
+    Config('thijswithaar/fedora:rawhide', 'Dockerfile.fedora_rawhide', 'Fedora'),
+    Config('thijswithaar/raspbian:buster', 'Dockerfile.raspbian', 'Raspbian.Buster'),
+)
+
+# Bootstrap the cloud-friendly cross-building environment:
+#run('cd xbuild; make docker')
 
 HOST_DIR = os.getcwd()
 DOCKER_BUILD_DIR='/home/docker/build/CD-Grab'
-for c in configs:
+for c in configs[2:]:
+    BUILD_DIR = f'{DOCKER_BUILD_DIR}/build.{c.DOCKER_SYSTEM}'
 
     print(f'\n** Building {c.DOCKER_IMAGE:s}')
-    run(f'docker stop {c.DOCKER_SYSTEM}')
-    run(f'docker container rm {c.DOCKER_SYSTEM}')
+    run(f'docker container rm -f {c.DOCKER_SYSTEM}')
     run(f'docker build -f {c.DOCKER_FILE:s} -t {c.DOCKER_IMAGE} .')
+    run(f'docker push {c.DOCKER_IMAGE}')
+
     run(f'docker run -it -d --user docker:docker -v {HOST_DIR}:{DOCKER_BUILD_DIR} -w {DOCKER_BUILD_DIR} --name {c.DOCKER_SYSTEM} {c.DOCKER_IMAGE}')
-    run(f'docker exec {c.DOCKER_SYSTEM} cmake -DCPACK_SYSTEM_NAME={c.DOCKER_SYSTEM} -DCMAKE_BUILD_TYPE=RELEASE -H{DOCKER_BUILD_DIR} -B{DOCKER_BUILD_DIR}/build.{c.DOCKER_SYSTEM}')
-    run(f'docker exec {c.DOCKER_SYSTEM} cmake --build {DOCKER_BUILD_DIR}/build.{c.DOCKER_SYSTEM} -- package')
+    #if c.DOCKER_FILE == 'Dockerfile.raspbian':
+    #    run(f'docker exec {c.DOCKER_SYSTEM} xbuild start')
+    run(f'docker exec {c.DOCKER_SYSTEM} cmake -GNinja -DCPACK_SYSTEM_NAME={c.DOCKER_SYSTEM} -DCMAKE_BUILD_TYPE=RELEASE -H{DOCKER_BUILD_DIR} -B{BUILD_DIR}')
+    run(f'docker exec {c.DOCKER_SYSTEM} cmake --build {BUILD_DIR} -- package')
