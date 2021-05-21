e4edda80fbe70e2d5aa8e37da6a2ff0f186de3b0
Thijs Withaar <thijs.withaar@gmail.com>
2021-05-21 12:18:19 +0200

    Appveyor: use vcpkg manifest, msvc 2019

---
+++ run_cmake.cmd
@@ -0,0 +1,17 @@
+@set SETUP_MSVC_2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+if not exist %SETUP_MSVC_2019% (
+	@set SETUP_MSVC_2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+SET VCPKG_ROOT=C:\build\vcpkg
+SET VCPKG_BINARY_SOURCES=clear;files,C:\build\vcpkg.cache,readwrite
+
+call %SETUP_MSVC_2019%
+cmake -G "Visual Studio 16 2019" -A x64 -T "v142" -DCMAKE_TOOLCHAIN_FILE=C:\build\vcpkg\scripts\buildsystems\vcpkg.cmake -H. -Bbuild.sln
+
+@rem cmake -G Ninja -A x64 -DCMAKE_CXX_COMPILER="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\Llvm\bin" -DCMAKE_TOOLCHAIN_FILE=C:\build\vcpkg\scripts\buildsystems\vcpkg.cmake -H. -Bbuild.llvm
+
+@rem cmake -G Ninja -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/Llvm/bin/clang-cl.exe" -DCMAKE_TOOLCHAIN_FILE=C:\build\vcpkg\scripts\buildsystems\vcpkg.cmake -H. -Bbuild.llvm
+
+@rem cmake --C  -H. -Bbuild.llvm
+
+pause
